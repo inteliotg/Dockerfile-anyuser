24be3366536bc007328736b0d9ba6479e7a9a8bf
AIBruceYin <113871653+AIBruceYin@users.noreply.github.com>
2022-12-16 10:24:11 +0800

    Update Hello-world.py

--- Hello-world.py
+++ Hello-world.py
@@ -4,13 +4,15 @@ import logging as log
 import sys
 
 log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
+log.info("Demo log output in container")
+log.info("--Hello Container--")
 
-log.info("Hello world")
-  
+log.info("Demo creating file in mount point")
 f = open('/app/mount-folder/data.txt', 'a+')
 f.write('Hello Python' + os.linesep)
 
+log.info("Demo keep the container live with max limitation")
 while True:
   time.sleep(5)
-  log.info("Hello world")
+  log.info("--Enter terminal to do what you want --")
 log.info("exit")
