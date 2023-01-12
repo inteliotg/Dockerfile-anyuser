9f14fa1b66d8d3576e3291ea664e2fc8bff65d8b
AIBruceYin <113871653+AIBruceYin@users.noreply.github.com>
2023-01-12 12:58:11 +0800

    Create Hello-Container.py

---
+++ Hello-Container.py
@@ -0,0 +1,19 @@
+import os
+import time
+import logging as log
+import sys
+
+log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
+log.info("Demo log output in container")
+log.info("--Hello Container--")
+
+log.info("Demo creating file in output mount point")
+f = open('./data.txt', 'a+')
+f.write('Hello Python' + os.linesep)
+log.info("Had created demo.txt file\n You can go to output point to check with terminal or filesystem.")
+
+log.info("Demo keep the container live with max limitation")
+while True:
+  time.sleep(5)
+  log.info("--Enter terminal to do what you want --")
+log.info("exit")
